85f226affc2ee30dd13f5c85683d552ebd46b9aa
Alexis Tyler <xo@wvvw.me>
2017-09-14 10:22:16 +0930

    add warning about invalid transport

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const transports = require('./transports');
 
 class Script {
-  constructor(config) {
-    config = Object.assign({}, config);
-    this._name = config.name || 'script';
+  constructor(config = {}) {
+    if (!this.transports.indexOf(config.transport)) {
+      throw new Error(`That transport isn't supported. Please choose one from ${this.transports}`);
+    }
+
+    this.transport = config.transport;
   }
   transports() {
     return Object.keys(transports);
